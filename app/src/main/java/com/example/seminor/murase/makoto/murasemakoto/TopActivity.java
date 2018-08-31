83d3a52ae102d6fe0db2ab4ba5193dea32bfb937
Minamikawa Hiroki <minamikawa_hiroki@intern.com>
2018-08-31 13:39:36 +0900

    add button event to intent.

--- app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/TopActivity.java
+++ app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/TopActivity.java
@@ -1,13 +1,30 @@
 package com.example.seminor.murase.makoto.murasemakoto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TopActivity extends AppCompatActivity {
+public class TopActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
+
+        Button btn_start = (Button)findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btn_start:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
