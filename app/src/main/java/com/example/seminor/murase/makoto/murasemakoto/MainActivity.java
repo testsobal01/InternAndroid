58258370240dcafca0adfdc384b15f0a85ff79be
ryoichi-shibata <ryoichi@intern.com>
2018-09-12 14:23:32 +0900

    kaoru murakami add option_menu tophe.

--- app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/MainActivity.java
+++ app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.seminor.murase.makoto.murasemakoto;
 
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,6 +32,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // 起動時に関数を呼び出す
         setQuestionValue();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.action_settings_top:
+                Intent homeIntent = new Intent(this, StartActivity.class);
+                this.startActivity(homeIntent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
