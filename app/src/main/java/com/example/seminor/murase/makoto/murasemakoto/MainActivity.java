c3999b627e89826f163ecfa9364a9b38ac0feeec
shumpeikato <shumpeikato@intern.com>
2018-09-12 14:22:52 +0900

    vib

--- app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/MainActivity.java
+++ app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.seminor.murase.makoto.murasemakoto;
 
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -34,19 +35,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         int id = view.getId();
+        Vibrator vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vib.vibrate(1000);
+
         switch (id) {
+
             case R.id.button1:
                 setAnswerValue();
                 checkResult(true);
+                vib.vibrate(1000);//バイブレーションがなる
+
                 break;
+
             case R.id.button2:
                 setAnswerValue();
                 checkResult(false);
+                vib.vibrate(1000);//バイブレーションがなる
+
                 break;
+
             case R.id.button3:
                 setQuestionValue();
                 clearAnswerValue();
                 clearScoreValue();
+                vib.cancel();//restartをおしたらバイブレーションがとまる
                 break;
         }
     }
@@ -144,7 +156,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void clearScoreValue() {
-        TextView txtScore = (TextView)findViewById(R.id.text_score);
+        TextView txtScore = (TextView) findViewById(R.id.text_score);
         txtScore.setText("0");
     }
 }
