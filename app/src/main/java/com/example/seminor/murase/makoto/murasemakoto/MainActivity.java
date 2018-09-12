8b9dddd7af7682ecf628f8df98e0ce7bcc0a125e
yuito narumi <yuito narumi@test.com>
2018-09-12 15:08:08 +0900

    create option menu

--- app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/MainActivity.java
+++ app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.seminor.murase.makoto.murasemakoto;
 
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,6 +32,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // 起動時に関数を呼び出す
         setQuestionValue();
+    }
+
+    // Option Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.goback:
+                // go back start view
+                Intent intent = new Intent(getApplicationContext(), StartScreen.class);
+                intent.putExtra("KEY", "MainActivitiyからの呼び出し");
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
