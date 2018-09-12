4968ccd06de640b8fa31f471c106aa54ca270285
Miyazawa Yuuki <MiyazawaYuuki@intern.com>
2018-09-12 13:55:05 +0900

    seve&bug fixed

--- app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/MainActivity.java
+++ app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     SharedPreferences pref;
     SharedPreferences.Editor prefEditor;
-    int savedScore=0;
+    int stopper=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,29 +61,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         int id = view.getId();
-        switch (id) {
-            case R.id.button1:
-                setAnswerValue();
-                checkResult(true);
-                break;
-            case R.id.button2:
-                setAnswerValue();
-                checkResult(false);
-                break;
-            case R.id.button3:
-                setQuestionValue();
-                clearAnswerValue();
-                clearScoreValue();
-                break;
+        if(stopper==0) {
+            switch (id) {
+                case R.id.button1:
+                    setAnswerValue();
+                    checkResult(true);
+                    break;
+                case R.id.button2:
+                    setAnswerValue();
+                    checkResult(false);
+                    break;
+                case R.id.button3:
+                    setQuestionValue();
+                    clearAnswerValue();
+                    clearScoreValue();
+                    break;
+            }
         }
     }
 
     private void setQuestionValue() {
         Random r = new Random();
-        // 0から10の範囲で乱数を生成（+1する必要がある）
-        int questionValue = r.nextInt(10 + 1);
-        TextView txtView = (TextView) findViewById(R.id.question);
-        txtView.setText(Integer.toString(questionValue));
+        if(stopper==0) {
+            // 0から10の範囲で乱数を生成（+1する必要がある）
+            int questionValue = r.nextInt(10 + 1);
+            TextView txtView = (TextView) findViewById(R.id.question);
+            txtView.setText(Integer.toString(questionValue));
+        }
     }
 
     private void setAnswerValue() {
@@ -100,44 +104,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int answer = Integer.parseInt(txtViewAnswer.getText().toString());
         TextView txtResult = (TextView) findViewById(R.id.text_result);
         // 結果を示す文字列を入れる変数を用意
-        String result;
+        String result="";
         int score = 0;
 
         // Highが押された
-        if (isHigh) {
-            // result には結果のみを入れる
-            if (question < answer) {
-                result = "WIN";
-                score = 2;
-            } else if (question > answer) {
-                result = "LOOSE";
-                score = -1;
+        if(stopper==0) {
+            if (isHigh) {
+                // result には結果のみを入れる
+                if (question < answer) {
+                    result = "WIN";
+                    score = 2;
+                } else if (question > answer) {
+                    result = "LOOSE";
+                    score = -1;
+                } else {
+                    result = "DRAW";
+                    score = 1;
+                }
             } else {
-                result = "DRAW";
-                score = 1;
-            }
-        } else {
-            if (question > answer) {
-                result = "WIN";
-                score = 2;
-            } else if (question < answer) {
-                result = "LOOSE";
-                score = -1;
-            } else {
-                result = "DRAW";
-                score = 1;
+                if (question > answer) {
+                    result = "WIN";
+                    score = 2;
+                } else if (question < answer) {
+                    result = "LOOSE";
+                    score = -1;
+                } else {
+                    result = "DRAW";
+                    score = 1;
+                }
             }
         }
 
-        // 最後にまとめてToast表示の処理とTextViewへのセットを行う
-        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
-        txtResult.setText("結果：" + question + ":" + answer + "(" + result + ")");
 
-        // 続けて遊べるように値を更新
-        setNextQuestion();
+            // 最後にまとめてToast表示の処理とTextViewへのセットを行う
+            Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+            txtResult.setText("結果：" + question + ":" + answer + "(" + result + ")");
 
-        // スコアを表示
-        setScore(score);
+            // 続けて遊べるように値を更新
+            setNextQuestion();
+
+            // スコアを表示
+            setScore(score);
+
     }
 
     private void clearAnswerValue() {
@@ -148,20 +156,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void setNextQuestion() {
         // 第１引数がカウントダウン時間、第２引数は途中経過を受け取る間隔
         // 単位はミリ秒（1秒＝1000ミリ秒）
-        new CountDownTimer(3000, 1000) {
+        if(stopper==0) {
+            stopper=1;
+            new CountDownTimer(3000, 1000) {
 
-            @Override
-            public void onTick(long l) {
-                // 途中経過を受け取った時に何かしたい場合
-                // 今回は特に何もしない
-            }
+                @Override
+                public void onTick(long l) {
+                    // 途中経過を受け取った時に何かしたい場合
+                    // 今回は特に何もしない
+                }
 
-            @Override
-            public void onFinish() {
-                // 3秒経過したら次の値をセット
-                setQuestionValue();
-            }
-        }.start();
+                @Override
+                public void onFinish() {
+                    // 3秒経過したら次の値をセット
+                    stopper=0;
+                    setQuestionValue();
+                    clearAnswerValue();
+                }
+            }.start();
+        }
     }
 
     private void setScore(int score) {
