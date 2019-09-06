4b5486f811e2d4a09671b0e8f2b3e14e35245de1
n923 <n923@sobal.co.jp>
2019-09-06 14:01:26 +0900

    shu-ito

--- app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/MainActivity.java
+++ app/src/main/java/com/example/seminor/murase/makoto/murasemakoto/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.seminor.murase.makoto.murasemakoto;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +31,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // 起動時に関数を呼び出す
         setQuestionValue();
+
     }
+
+
 
     @Override
     public void onClick(View view) {
