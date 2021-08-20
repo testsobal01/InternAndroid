324a0cbec49e87afde492f84fe63d7a883935b27
n916 <n916@sobal.co.jp>
2021-08-20 11:34:39 +0900

    app/change app name.

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -45,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 checkResult(false);
                 break;
             case R.id.button3:
+                Biv(3);
                 setQuestionValue();
                 clearAnswerValue();
                 break;
@@ -87,9 +89,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (isHigh) {
             // result には結果のみを入れる
             if (question < answer) {
+                Biv(1);
                 result = "WIN";
                 score = 2;
             } else if (question > answer) {
+                Biv(2);
                 result = "LOSE";
                 score = -1;
             } else {
@@ -98,9 +102,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         } else {
             if (question > answer) {
+                Biv(1);
                 result = "WIN";
                 score = 2;
             } else if (question < answer) {
+                Biv(2);
                 result = "LOSE";
                 score = -1;
             } else {
@@ -145,6 +151,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TextView txtScore = (TextView) findViewById(R.id.text_score);
         int newScore = Integer.parseInt(txtScore.getText().toString()) + score;
         txtScore.setText(Integer.toString(newScore));
+    }
+
+    //* ------------------------------- *//
+    // 　　　バイブレーション機能             //
+    //                                　 //
+    //      WINの時、0.0８秒バイブ          //
+    //      LOSEの時、０.32秒バイブ         //
+    //      DRAWの時、バイブなし            //
+    //      RESTARTの時、１秒バイブ         //
+    //* ------------------------------　*//
+    private void Biv(int i) {
+        Vibrator vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+            // WINの時
+        if (i == 1) {
+            vib.vibrate(80);
+            //LOSEの時
+        } else if (i == 2) {
+            vib.vibrate(320);
+            // RESTARTの時
+        } else if (i == 3) {
+            vib.vibrate(1000);
+        }
     }
 
 }
