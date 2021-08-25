3d41a8f2f293ff8da0e6661aae4560a04dd5baf6
n930 <n930@sobal.co.jp>
2021-08-25 11:19:36 +0900

    add topscreen

---
+++ app/src/main/java/com/example/makotomurase/StartActivity.java
@@ -0,0 +1,31 @@
+package com.example.makotomurase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.button1:
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+    }
+}
