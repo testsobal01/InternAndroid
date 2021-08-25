680589d707afe0d86af336dd947d9bfb202fa5e1
n948 <n948@sobal.co.jp>
2021-08-25 10:54:04 +0900

    add vibrate.

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -35,16 +36,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         int id = view.getId();
+        Vibrator vib = (Vibrator)getSystemService(VIBRATOR_SERVICE);
         switch (id) {
             case R.id.button1:
+                vib.vibrate(1000);
                 setAnswerValue();
                 checkResult(true);
                 break;
             case R.id.button2:
+                vib.vibrate(2000);
                 setAnswerValue();
                 checkResult(false);
                 break;
             case R.id.button3:
+                vib.vibrate(3000);
                 setQuestionValue();
                 clearAnswerValue();
                 break;
