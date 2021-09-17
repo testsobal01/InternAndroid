fe2d48d2b0ba7ef76cc9961a455f377a488aa297
n911 <n911@sobal.co.jp>
2021-09-17 11:10:00 +0900

    add vibrator.

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         int id = view.getId();
+        vibrator();
         switch (id) {
             case R.id.button1:
                 setAnswerValue();
@@ -48,9 +50,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 setQuestionValue();
                 clearAnswerValue();
                 break;
-
         }
 
+    }
+
+    private void vibrator(){
+        Vibrator vib = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+        vib.vibrate(100);
     }
 
     private void clearAnswerValue() {
