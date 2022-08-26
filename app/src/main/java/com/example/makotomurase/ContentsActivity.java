6be61c3d91db19c5dbd472c936eda5cd97b1a9d2
ryusuke_nagai <kairi.satellite@gmail.com>
2022-08-26 13:17:42 +0900

    トップ画面を追加しました

---
+++ app/src/main/java/com/example/makotomurase/ContentsActivity.java
@@ -0,0 +1,32 @@
+package com.example.makotomurase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ContentsActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contents);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (view.getId()) {
+            case R.id.button:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
