9fc219bec06a232dcbfa1f06b46083587431b12f
n926 <n926@sobal.co.jp>
2022-08-26 11:08:11 +0900

    バイブレーション

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -34,15 +35,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        Vibrator vib=(Vibrator)getSystemService(VIBRATOR_SERVICE);
         int id = view.getId();
         switch (id) {
             case R.id.button1:
                 setAnswerValue();
                 checkResult(true);
+                vib.vibrate(5000);
                 break;
             case R.id.button2:
                 setAnswerValue();
                 checkResult(false);
+                vib.vibrate(5000);
                 break;
             case R.id.button3:
                 setQuestionValue();
