2b5b8b7052d7727bb7000eb0ae64b76fc5c08df5
n936 <n936@sobal.co.jp>
2022-08-31 12:18:04 +0900

    多言語対応

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
