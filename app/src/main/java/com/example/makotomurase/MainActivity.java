4c8626a9e2f5d7f69ff1b5b22b9a8c3bdece3aa3
n911 <n911@sobal.co.jp>
2022-08-31 11:45:24 +0900

    socre

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.makotomurase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -12,6 +13,9 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor prefEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 起動時に関数を呼び出す
         setQuestionValue();
 
+        pref = getSharedPreferences("score", MODE_PRIVATE);
+        prefEditor = pref.edit();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        TextView textView = (TextView)findViewById(R.id.text_score);
+        String readText = pref.getString("main_input", "保存されていません");
+        textView.setText(readText);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //画面上の文字列を保存するため、テキストビューを取得
+        TextView textView = (TextView)findViewById(R.id.text_score);
+        //"main_input"というキー名（箱）に、文字列を保存
+        prefEditor.putInt("main_input", Integer.parseInt(textView.getText().toString()));
+        prefEditor.commit();
     }
 
     @Override
