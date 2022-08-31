da4f7a2df82ec573f22b1af6d78463f803466f6b
n921 <n921@sobal.co.jp>
2022-08-31 11:17:08 +0900

    add score preference.

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.makotomurase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -13,6 +14,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private SharedPreferences data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +32,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 起動時に関数を呼び出す
         setQuestionValue();
 
+        //Preferenceから値を読み込む
+        data = getSharedPreferences("Data", MODE_PRIVATE);
+        int score = data.getInt("score", 0);
+        setScore(score);
     }
 
     @Override
@@ -145,6 +151,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TextView txtScore = (TextView) findViewById(R.id.text_score);
         int newScore = Integer.parseInt(txtScore.getText().toString()) + score;
         txtScore.setText(Integer.toString(newScore));
+
+        SharedPreferences.Editor editor = data.edit();//スコアをPreferenceに記録
+        editor.putInt("score", newScore);
+        editor.apply();
     }
 
 }
