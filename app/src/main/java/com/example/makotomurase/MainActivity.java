783878bc4e10289fba7321df88a7adef486fc8de
n1467 <n1467@sobal.co.jp>
2023-08-09 14:26:33 +0900

    タイマーのきっかけをボタンクリックではなく

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +29,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // 起動時に関数を呼び出す
         setQuestionValue();
+
+
+        setNextQuestion();
+
+            
     }
 
     @Override
@@ -116,12 +120,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtResult.setText(getString(R.string.result) + question + ":" + answer + "(" + result + ")");
 
         // 続けて遊べるように値を更新
-        setNextQuestion();
+
         // スコアを表示
         setScore(score);
     }
 
     private void setNextQuestion() {
+        // 第１引数がカウントダウン時間、第２引数は途中経過を受け取る間隔
+        // 単位はミリ秒（1秒＝1000ミリ秒）
+            new CountDownTimer(3000, 1000) {
+                @Override
+                public void onTick(long l) {
+                    // 途中経過を受け取った時に何かしたい場合
+                    // 今回は特に何もしない
+                }
+
+                @Override
+                public void onFinish() {
+                    // 3秒経過したら次の値をセット
+                    setQuestionValue();
+                    setNextQuestion2();
+                }
+            }.start();
+
+    }
+    private void setNextQuestion2() {
         // 第１引数がカウントダウン時間、第２引数は途中経過を受け取る間隔
         // 単位はミリ秒（1秒＝1000ミリ秒）
         new CountDownTimer(3000, 1000) {
@@ -135,6 +158,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void onFinish() {
                 // 3秒経過したら次の値をセット
                 setQuestionValue();
+                setNextQuestion();
             }
         }.start();
     }
