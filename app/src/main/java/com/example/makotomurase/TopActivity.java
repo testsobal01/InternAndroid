10797fff9ee4344327fbdb0925743ead9ebbdc3f
n1497 <n1497@sobal.co.jp>
2023-08-09 13:24:31 +0900

    トップ画面追加

--- app/src/main/java/com/example/makotomurase/TopActivity.java
+++ app/src/main/java/com/example/makotomurase/TopActivity.java
@@ -2,13 +2,33 @@ package com.example.makotomurase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class TopActivity extends AppCompatActivity {
+public class TopActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
+
+        Button TopButton = findViewById(R.id.top_button);
+        TopButton.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View view){
+        //view（buttonの中身）を格納する変数id
+        int id = view.getId();
+        if(id == R.id.top_button){
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
