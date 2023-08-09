4c4ad7d68b79a9fc9718973ae8e50ab9f9686b36
n1487 <n1487@sobal.co.jp>
2023-08-09 13:40:43 +0900

    フッター追加

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // 起動時に関数を呼び出す
         setQuestionValue();
+
+        ImageView imageView = findViewById(R.id.footer);
+        imageView.setImageResource(R.drawable.footer);
     }
 
     @Override
