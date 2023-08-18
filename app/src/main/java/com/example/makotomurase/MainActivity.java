fa16afab48573fd8861537fb5007f590e866da48
n1466 <n1466@sobal.co.jp>
2023-08-18 10:22:16 +0900

    機能1：トップ画面（アクティビティ）追加

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.makotomurase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
