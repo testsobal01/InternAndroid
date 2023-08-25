9bd266d63f780678f40168d89a6c0c6bb3309997
n482 <n482@sobal.co.jp>
2023-08-25 11:34:26 +0900

    起動画面の実装と設定画面の実装

---
+++ app/src/main/java/com/example/makotomurase/Constrants.java
@@ -0,0 +1,10 @@
+package com.example.makotomurase;
+
+public class Constrants {
+    public enum START_OPTION {
+        NEW, CONTINUE
+    }
+    public final static String KEY_INTENT_START_OPTION = "start_option";
+    public static final int RANGE_RANDOM_MAX = 50;
+    public static final int RANGE_RANDOM_MIN = 10;
+}
