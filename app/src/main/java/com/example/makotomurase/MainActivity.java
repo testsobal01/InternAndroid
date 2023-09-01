4321c5b1af80728f2e62bd4e39c9c12f9fa2cf2f
m477 <m477@sobal.co.jp>
2023-09-01 14:00:55 +0900

    アニメーションの実装

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Vibrator;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +15,15 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public void blink(TextView txt,long duration,long offset){
+        Animation anm = new AlphaAnimation(0.0f,1.0f);
+        anm.setDuration(duration);
+        anm.setStartOffset(offset);
+        anm.setRepeatMode(Animation.REVERSE);
+        anm.setRepeatCount(0);
+        txt.startAnimation(anm);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +83,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtView.setText(Integer.toString(answerValue));
     }
 
+    private void Qblink(){
+        TextView Qtxt = (TextView)findViewById(R.id.question);
+        blink(Qtxt,10,500);
+    }
+
+    private void Ablink(){
+        TextView Atxt = (TextView)findViewById(R.id.answer);
+        blink(Atxt,10,500);
+    }
+
     private void checkResult(boolean isHigh) {
         TextView txtViewQuestion = findViewById(R.id.question);
         TextView txtViewAnswer = findViewById(R.id.answer);
@@ -84,15 +105,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 結果を示す文字列を入れる変数を用意
         String result;
         int score;
-
         // Highが押された
         if (isHigh) {
             // result には結果のみを入れる
             if (question < answer) {
                 result = "WIN";
+                Ablink();
                 score = 2;
             } else if (question > answer) {
                 result = "LOSE";
+                Qblink();
                 score = -1;
             } else {
                 result = "DRAW";
@@ -101,9 +123,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             if (question > answer) {
                 result = "WIN";
+                Ablink();
                 score = 2;
             } else if (question < answer) {
                 result = "LOSE";
+                Qblink();
                 score = -1;
             } else {
                 result = "DRAW";
