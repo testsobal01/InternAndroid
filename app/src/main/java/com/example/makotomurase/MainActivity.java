7ca972a4b9b23c04ab4f84edd4f0490c7ad4e1c7
n1472 <n1472@sobal.co.jp>
2024-08-09 11:37:44 +0900

    数字にアニメーションを追加しました

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.Transformation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -111,6 +117,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 最後にまとめてToast表示の処理とTextViewへのセットを行う
         Toast.makeText(this, result, Toast.LENGTH_LONG).show();
         txtResult.setText("結果：" + question + ":" + answer + "(" + result + ")");
+        b_7(result);
+
 
         // 続けて遊べるように値を更新
         setNextQuestion();
@@ -145,6 +153,151 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void clearScoreValue() {
         TextView txtScore = (TextView) findViewById(R.id.text_score);
         txtScore.setText("0");
+    }
+
+    private void b_7(String result) {//勝敗によって数字が動く機能
+        TextView txtViewQuestion = findViewById(R.id.question);
+        TextView txtViewAnswer = findViewById(R.id.answer);
+
+        TranslateAnimation TranslateQs = new TranslateAnimation(
+                Animation.ABSOLUTE, 0f,
+                Animation.ABSOLUTE, 125f,
+                Animation.ABSOLUTE, 0f,
+                Animation.ABSOLUTE, 0f);
+        TranslateQs.setDuration(500);
+        TranslateQs.setRepeatCount(0);
+        TranslateQs.setFillAfter(false);
+        TranslateAnimation TranslateQf = new TranslateAnimation(
+                Animation.ABSOLUTE, 125f,
+                Animation.ABSOLUTE, 0f,
+                Animation.ABSOLUTE, 0f,
+                Animation.ABSOLUTE, 0f);
+        TranslateQf.setDuration(1000);
+        TranslateQf.setRepeatCount(0);
+        TranslateQf.setFillAfter(false);
+        TranslateAnimation TranslateQl = new TranslateAnimation(
+                Animation.ABSOLUTE, 125f,
+                Animation.ABSOLUTE, -1500f,
+                Animation.ABSOLUTE, 0f,
+                Animation.ABSOLUTE, 4000f);
+        TranslateQl.setDuration(1000);
+        TranslateQl.setRepeatCount(0);
+        TranslateQl.setFillAfter(false);
+        TranslateAnimation TranslateAs = new TranslateAnimation(
+                Animation.ABSOLUTE, 0f,
+                Animation.ABSOLUTE, -125f,
+                Animation.ABSOLUTE, 0f,
+                Animation.ABSOLUTE, 0f);
+        TranslateAs.setDuration(500);
+        TranslateAs.setRepeatCount(0);
+        TranslateAs.setFillAfter(false);
+        TranslateAnimation TranslateAf = new TranslateAnimation(
+                Animation.ABSOLUTE, -125f,
+                Animation.ABSOLUTE, 0f,
+                Animation.ABSOLUTE, 0f,
+                Animation.ABSOLUTE, 0f);
+        TranslateAf.setDuration(1000);
+        TranslateAf.setRepeatCount(0);
+        TranslateAf.setFillAfter(false);
+        TranslateAnimation TranslateAl = new TranslateAnimation(
+                Animation.ABSOLUTE, -125f,
+                Animation.ABSOLUTE, 1500f,
+                Animation.ABSOLUTE, 0f,
+                Animation.ABSOLUTE, 4000f);
+        TranslateAl.setDuration(1000);
+        TranslateAl.setRepeatCount(0);
+        TranslateAl.setFillAfter(false);
+
+        RotateAnimation RotateWq = new RotateAnimation(
+                0.0f, -50f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        RotateWq.setDuration(1000);
+        RotateWq.setRepeatCount(0);
+        RotateWq.setFillAfter(false);
+        RotateAnimation RotateWa = new RotateAnimation(
+                0.0f, 50f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        RotateWa.setDuration(1000);
+        RotateWa.setRepeatCount(0);
+        RotateWa.setFillAfter(false);
+
+        ScaleAnimation scaleS = new ScaleAnimation(
+                1.0f, 0.7f, 1.0f, 0.7f,
+                Animation.RELATIVE_TO_SELF, 0.7f,
+                Animation.RELATIVE_TO_SELF, 0.7f);
+        scaleS.setDuration(500);
+        scaleS.setRepeatCount(0);
+        scaleS.setFillAfter(false);
+        ScaleAnimation scaleW = new ScaleAnimation(
+                0.7f, 0.4f, 0.7f, 0.4f,
+                Animation.RELATIVE_TO_SELF, 0.7f,
+                Animation.RELATIVE_TO_SELF, 0.7f);
+        scaleW.setDuration(1000);
+        scaleW.setRepeatCount(0);
+        scaleW.setFillAfter(false);
+        ScaleAnimation scaleL = new ScaleAnimation(
+                0.7f, 0.2f, 0.7f, 0.2f,
+                Animation.RELATIVE_TO_SELF, 0.7f,
+                Animation.RELATIVE_TO_SELF, 0.7f);
+        scaleL.setDuration(1000);
+        scaleL.setRepeatCount(0);
+        scaleL.setFillAfter(false);
+        ScaleAnimation scaleD = new ScaleAnimation(
+                0.7f, 0.8f, 0.7f, 0.8f,
+                Animation.RELATIVE_TO_SELF, 0.7f,
+                Animation.RELATIVE_TO_SELF, 0.7f);
+        scaleD.setDuration(1000);
+        scaleD.setRepeatCount(0);
+        scaleD.setFillAfter(false);
+
+        AnimationSet startQ = new AnimationSet(true);
+        startQ.addAnimation(TranslateQs);
+        startQ.addAnimation(scaleS);
+        AnimationSet startA = new AnimationSet(true);
+        startA.addAnimation(TranslateAs);
+        startA.addAnimation(scaleS);
+        AnimationSet QW = new AnimationSet(true);
+        QW.addAnimation(TranslateQf);
+        QW.addAnimation(RotateWq);
+        QW.addAnimation(scaleD);
+        AnimationSet AW = new AnimationSet(true);
+        AW.addAnimation(TranslateAf);
+        AW.addAnimation(RotateWa);
+        AW.addAnimation(scaleD);
+        AnimationSet QL = new AnimationSet(true);
+        QL.addAnimation(TranslateQl);
+        QL.addAnimation(scaleL);
+        AnimationSet AL = new AnimationSet(true);
+        AL.addAnimation(TranslateAl);
+        AL.addAnimation(scaleL);
+        AnimationSet QD = new AnimationSet(true);
+        QD.addAnimation(TranslateQf);
+        QD.addAnimation(scaleD);
+        AnimationSet AD = new AnimationSet(true);
+        AD.addAnimation(TranslateAf);
+        AD.addAnimation(scaleD);
+
+        txtViewQuestion.startAnimation(startQ);
+        txtViewAnswer.startAnimation(startA);
+        new CountDownTimer(500, 1000) {
+            @Override
+            public void onTick(long l) {}
+            @Override
+            public void onFinish() {
+                if (result == "WIN") {
+                    txtViewQuestion.startAnimation(QW);
+                    txtViewAnswer.startAnimation(AW);
+                } else if (result == "LOSE") {
+                    txtViewQuestion.startAnimation(QL);
+                    txtViewAnswer.startAnimation(AL);
+                } else if (result == "DRAW") {
+                    txtViewQuestion.startAnimation(QD);
+                    txtViewAnswer.startAnimation(AD);
+                }
+            }
+        }.start();
     }
 }
 
