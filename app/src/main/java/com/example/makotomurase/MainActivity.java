ed605c0d3d4ccd6b89c32ba6cbb9a98052e81601
n1497 <n1497@sobal.co.jp>
2024-08-22 14:16:29 +0900

    プリファレンスにスコア保存

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.makotomurase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -12,6 +13,9 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor prefEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // 起動時に関数を呼び出す
         setQuestionValue();
+
+        pref = getSharedPreferences("MakotoMurase",MODE_PRIVATE);
+        prefEditor = pref.edit();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
+        //scoreの保存
+        TextView textView = (TextView) findViewById(R.id.text_score);//保存するscoreを取得
+        prefEditor.putString("score_input",textView.getText().toString());
+        prefEditor.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show();
+        TextView textView = (TextView) findViewById(R.id.text_score);
+        String readText = pref.getString("score_input","保存されていません");
+        textView.setText(readText);
     }
 
     @Override
