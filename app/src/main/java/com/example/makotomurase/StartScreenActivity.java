618dc853dedfc42bd12e94f6a1053399fc59c467
n1502 <n1502@sobal.co.jp>
2024-08-22 11:41:07 +0900

    お昼前の一時的な保存

---
+++ app/src/main/java/com/example/makotomurase/StartScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.makotomurase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
