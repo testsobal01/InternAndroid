4359bf98b0f97105b316b9e65b2be0716f25aa8b
n1472 <testsobal02@gmail.com>
2024-08-22 14:39:46 +0900

    1.トップ画面追加

--- app/src/main/java/com/example/makotomurase/TitleActivity.java
+++ app/src/main/java/com/example/makotomurase/TitleActivity.java
@@ -2,13 +2,28 @@ package com.example.makotomurase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TitleActivity extends AppCompatActivity {
+public class TitleActivity extends AppCompatActivity implements View.OnClickListener    {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
+        Button btn4 = findViewById(R.id.start_button);
+        btn4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.start_button){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
