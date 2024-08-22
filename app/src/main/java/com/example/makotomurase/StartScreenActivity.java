3a7b8b0c431db10adb89f957c77de1953983885e
n1502 <n1502@sobal.co.jp>
2024-08-22 13:13:10 +0900

    画面遷移の機能はできた…！やった！次は、UIに注力します。

--- app/src/main/java/com/example/makotomurase/StartScreenActivity.java
+++ app/src/main/java/com/example/makotomurase/StartScreenActivity.java
@@ -2,13 +2,28 @@ package com.example.makotomurase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartScreenActivity extends AppCompatActivity {
+public class StartScreenActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        Button btn_start = findViewById(R.id.button_start);
+        btn_start.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.button_start) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
