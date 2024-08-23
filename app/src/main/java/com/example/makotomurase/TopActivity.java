d7506c07040c83766e39f805e4255cb458923106
n1487 <n1487@sobal.co.jp>
2024-08-23 13:06:39 +0900

    11-1 title

--- app/src/main/java/com/example/makotomurase/TopActivity.java
+++ app/src/main/java/com/example/makotomurase/TopActivity.java
@@ -1,7 +1,9 @@
 package com.example.makotomurase;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +20,9 @@ public class TopActivity extends AppCompatActivity implements View.OnClickListen
 
         Button startbutton = findViewById(R.id.startbutton);
         startbutton.setOnClickListener(this);
-        //startbutton.setBackground(getResources().getDrawable(R.drawable.button_top_start,null));
+
+        Button introbutton = findViewById(R.id.introbutton);
+        introbutton.setOnClickListener(this);
     }
 
     public void onStartButtonClick(View view){
@@ -31,6 +35,17 @@ public class TopActivity extends AppCompatActivity implements View.OnClickListen
         if(id == R.id.startbutton){
             Intent intent = new Intent(this,MainActivity.class);
             startActivity(intent);
+        } else if(id == R.id.introbutton){
+            AlertDialog.Builder builder = new AlertDialog.Builder(TopActivity.this);
+            builder.setTitle("遊び方");
+            builder.setMessage(" 右に表示される値が左の値よりも上(大きい)か下(小さい)かあてよう。画面上部にあるボタンをおしてね。数字は0から10まで表示されるよ。リスタートボタンでゲームをリセットしてやり直しできるよ。");
+            builder.setPositiveButton(" ", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            builder.show();
         }
     }
 }
