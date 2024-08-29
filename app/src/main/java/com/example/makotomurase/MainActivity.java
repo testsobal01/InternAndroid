55b0eae305baf2bdbcb4ff6ddc5a42c076328581
n1478 <n1478@sobal.co.jp>
2024-08-29 13:03:02 +0900

    コミットメッセージ

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -146,5 +146,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TextView txtScore = (TextView) findViewById(R.id.text_score);
         txtScore.setText("0");
     }
+    
 }
 
