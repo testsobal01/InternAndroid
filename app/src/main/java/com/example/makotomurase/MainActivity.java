57a58edd6d4e488f67a70d43667cd4e7c30235b5
n1496 <n1496@sobal.co.jp>
2025-08-08 10:47:09 +0900

    koukaon1igai

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.makotomurase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -13,6 +16,18 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    int mp3b;
+    int mp3c;
+    int mp3d;
+    int mp3e;
+    int mp3f;
+    int mp3g;
+    SoundPool soundPool2;
+    SoundPool soundPool3;
+    SoundPool soundPool4;
+    SoundPool soundPool5;
+    SoundPool soundPool6;
+    SoundPool soundPool7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +42,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btn3 = (Button) findViewById(R.id.button3);
         btn3.setOnClickListener(this);
 
+
         // 起動時に関数を呼び出す
         setQuestionValue();
+
+        AudioAttributes attr = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+
+        soundPool2 = new SoundPool.Builder()
+                .setAudioAttributes(attr)
+                .setMaxStreams(2)
+                .build();
+        soundPool3 = new SoundPool.Builder()
+                .setAudioAttributes(attr)
+                .setMaxStreams(2)
+                .build();
+        soundPool4 = new SoundPool.Builder()
+                .setAudioAttributes(attr)
+                .setMaxStreams(2)
+                .build();
+        soundPool5 = new SoundPool.Builder()
+                .setAudioAttributes(attr)
+                .setMaxStreams(2)
+                .build();
+        soundPool6 = new SoundPool.Builder()
+                .setAudioAttributes(attr)
+                .setMaxStreams(2)
+                .build();
+        soundPool7 = new SoundPool.Builder()
+                .setAudioAttributes(attr)
+                .setMaxStreams(2)
+                .build();
+
+        mp3b = soundPool2.load(this, R.raw.high, 1);
+        mp3c = soundPool3.load(this, R.raw.row, 1);
+        mp3d = soundPool4.load(this, R.raw.tyantyan3, 1);
+        mp3e = soundPool5.load(this, R.raw.win, 1);
+        mp3f = soundPool6.load(this, R.raw.lose1, 1);
+        mp3g = soundPool7.load(this, R.raw.drow, 1);
+
+
     }
 
     @Override
     public void onClick(View view) {
+//        soundPool=new SoundPool(2, AudioManager.STREAM_MUSIC,0);
+
+
+
         int id = view.getId();
         if (id == R.id.button1) {
             setAnswerValue();
@@ -44,6 +103,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             setQuestionValue();
             clearAnswerValue();
             clearScoreValue();
+            soundPool4.play(mp3d,1f , 1f, 0, 0, 1f);
+
         }
     }
 
@@ -82,29 +143,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String result;
         int score;
 
+
         // Highが押された
         if (isHigh) {
+            soundPool2.play(mp3b,1f , 1f, 0, 0, 1f);
+
             // result には結果のみを入れる
             if (question < answer) {
                 result = "WIN";
                 score = 2;
+                soundPool5.play(mp3e,1f , 1f, 0, 0, 1f);
             } else if (question > answer) {
                 result = "LOSE";
                 score = -1;
+                soundPool6.play(mp3f,1f , 1f, 0, 0, 1f);
             } else {
                 result = "DRAW";
                 score = 1;
+                soundPool7.play(mp3g,1f , 1f, 0, 0, 1f);
             }
         } else {
+            soundPool3.play(mp3c,1f , 1f, 0, 0, 1f);
+
             if (question > answer) {
                 result = "WIN";
                 score = 2;
+                soundPool5.play(mp3e,2f , 2f, 0, 0, 1f);
             } else if (question < answer) {
                 result = "LOSE";
                 score = -1;
+                soundPool6.play(mp3f,1f , 1f, 0, 0, 1f);
             } else {
                 result = "DRAW";
                 score = 1;
+                soundPool7.play(mp3g,1f , 1f, 0, 0, 1f);
+
             }
         }
 
