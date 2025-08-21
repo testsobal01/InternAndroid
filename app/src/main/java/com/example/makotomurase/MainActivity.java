01a0f99aaba8afbc148103d61606f90de84579a0
n1510 <n1510@sobal.co.jp>
2025-08-21 15:45:52 +0900

    トップ画面追加

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.makotomurase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -27,13 +28,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btn3 = (Button) findViewById(R.id.button3);
         btn3.setOnClickListener(this);
 
+        Intent intent = getIntent();
+        Bundle extra = intent.getExtras();
+
+
+
         // 起動時に関数を呼び出す
         setQuestionValue();
     }
 
+    protected void onResume(){
+        super.onResume();
+
+    }
     @Override
     public void onClick(View view) {
         int id = view.getId();
+
+
         if (id == R.id.button1) {
             setAnswerValue();
             checkResult(true);
