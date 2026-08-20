0cab816228b8c01f9349ec26a5849a2d028c988e
n1484 <n1484@sobal.co.jp>
2026-08-20 14:04:01 +0900

    勝敗で背景色変更

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -93,29 +94,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String result;
         int score;
 
+        TextView colorChange1 = (TextView) findViewById(R.id.question);
+        TextView colorChange2 = (TextView) findViewById(R.id.answer);
+
+
         // Highが押された
         if (isHigh) {
             // result には結果のみを入れる
             if (question < answer) {
                 result = "WIN";
                 score = 2;
+                colorChange1.setBackgroundColor(Color.RED);
+                colorChange2.setBackgroundColor(Color.RED);
             } else if (question > answer) {
                 result = "LOSE";
                 score = -1;
+                colorChange1.setBackgroundColor(Color.BLUE);
+                colorChange2.setBackgroundColor(Color.BLUE);
             } else {
                 result = "DRAW";
                 score = 1;
+                colorChange1.setBackgroundColor(Color.GRAY);
+                colorChange2.setBackgroundColor(Color.GRAY);
             }
         } else {
             if (question > answer) {
                 result = "WIN";
                 score = 2;
+                colorChange1.setBackgroundColor(Color.RED);
+                colorChange2.setBackgroundColor(Color.RED);
             } else if (question < answer) {
                 result = "LOSE";
                 score = -1;
+                colorChange1.setBackgroundColor(Color.BLUE);
+                colorChange2.setBackgroundColor(Color.BLUE);
             } else {
                 result = "DRAW";
                 score = 1;
+                colorChange1.setBackgroundColor(Color.GRAY);
+                colorChange2.setBackgroundColor(Color.GRAY);
             }
         }
 
