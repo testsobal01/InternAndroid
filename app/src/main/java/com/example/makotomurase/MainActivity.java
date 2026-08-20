48a3dee6c7a1c05639a72ecc238dc957f0ca13e4
n1495 <n1495@sobal.co.jp>
2026-08-20 13:42:47 +0900

    プリファレンスにスコア保存

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +17,9 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor prefEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btn3 = (Button) findViewById(R.id.button3);
         btn3.setOnClickListener(this);
+
+        pref=getSharedPreferences("AndroidSeminar",MODE_PRIVATE);
+        prefEditor=pref.edit();
 
         // 起動時に関数を呼び出す
         setQuestionValue();
@@ -156,6 +164,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void clearScoreValue() {
         TextView txtScore = (TextView) findViewById(R.id.text_score);
         txtScore.setText("0");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
+
+        TextView textView=(TextView) findViewById (R.id.text_score);
+        int num = Integer.parseInt(textView.getText().toString());
+
+        prefEditor.putInt("main_input",num);
+        prefEditor.commit();
+    }
+
+    @Override
+
+    protected void onResume(){
+        super.onResume();
+        Log.d("AndroidTest","onResume completed.");
+
+        TextView textView=(TextView) findViewById(R.id.text_score);
+        int num = Integer.parseInt(textView.getText().toString());
+
+        int readText =pref.getInt("main_input",num);
+        textView.setText(Integer.toString(readText));
     }
 }
 
