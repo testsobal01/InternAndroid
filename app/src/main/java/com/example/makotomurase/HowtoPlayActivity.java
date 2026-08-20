49f750336f2635fe7a57d5abbe98df6acf7b2fb1
n1480 <n1480@sobal.co.jp>
2026-08-20 15:52:44 +0900

    遊び方ページの追加

--- app/src/main/java/com/example/makotomurase/HowtoPlayActivity.java
+++ app/src/main/java/com/example/makotomurase/HowtoPlayActivity.java
@@ -1,7 +1,6 @@
 package com.example.makotomurase;
 
 import android.content.Intent;
-import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TitleActivity extends AppCompatActivity implements View.OnClickListener{
+public class HowtoPlayActivity extends AppCompatActivity implements View.OnClickListener {
 
     private SoundPlayer soundPlayer;
 
@@ -20,32 +19,25 @@ public class TitleActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_title_actibity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.title), (v, insets) -> {
+        setContentView(R.layout.activity_howto_play);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.howto), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button button_title = findViewById(R.id.button_start);
-        button_title.setOnClickListener(this);
-        Button button_howto = findViewById(R.id.button_howto);
-        button_howto.setOnClickListener(this);
-
+        Button button_back = findViewById(R.id.button_back);
+        button_back.setOnClickListener(this);
 
         soundPlayer = new SoundPlayer(this);
     }
 
     @Override
     public void onClick(View view) {
-    int id = view.getId();
-    if (id == R.id.button_start) {
-        soundPlayer.playtitleSound();
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    } else if (id == R.id.button_howto) {
-        soundPlayer.playtitleSound();
-        Intent intent = new Intent(this, HowtoPlayActivity.class);
-        startActivity(intent);
-    }
+        int id = view.getId();
+        if (id == R.id.button_back) {
+            soundPlayer.playtitleSound();
+            Intent intent = new Intent(this, TitleActivity.class);
+            startActivity(intent);
+        }
     }
 }
