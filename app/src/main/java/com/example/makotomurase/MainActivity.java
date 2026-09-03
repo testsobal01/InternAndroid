7c741a2f95e953db4725e2705404e44f87ea3073
n-1507 <n-1507@sobal.co.jp>
2026-09-03 14:17:32 +0900

    スコア保存機能の追加

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -15,6 +16,9 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor prefEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btn3 = (Button) findViewById(R.id.button3);
         btn3.setOnClickListener(this);
 
+        pref = getSharedPreferences("SaveValue", MODE_PRIVATE);
+        prefEditor = pref.edit();
+
         // 起動時に関数を呼び出す
         setQuestionValue();
     }
@@ -56,6 +63,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             clearAnswerValue();
             clearScoreValue();
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        int savevalue = 0;
+        TextView SaveScore = (TextView) findViewById(R.id.text_score);
+        String savescore = (String) SaveScore.getText();
+        try {
+            savevalue = Integer.parseInt(savescore);
+        } catch (NumberFormatException e) {
+            System.err.println("数値に変換できません： " + e.getMessage());
+        }
+
+        prefEditor.putInt("Save", savevalue);
+        prefEditor.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int readvalue = pref.getInt("Save", 0);
+        TextView ReadScore = (TextView) findViewById(R.id.text_score);
+        ReadScore.setText(Integer.toString(readvalue));
     }
 
     private void clearAnswerValue() {
