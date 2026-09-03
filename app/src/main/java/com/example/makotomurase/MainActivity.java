7a0f85f0b1a1fcc2ecbcc02fa04b085cbc1643ee
n1477 <n1477@sobal.co.jp>
2026-09-03 14:21:14 +0900

    背景の色を変える

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -5,16 +5,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.app.ActivityManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,8 +81,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         TextView txtView = findViewById(R.id.answer);
         txtView.setText(Integer.toString(answerValue));
+
     }
 
+    public int generateRandomWarmColor() {
+        Random random = new Random();
+        int red = random.nextInt(106) + 150;
+        int green = random.nextInt(151) + 50;
+        int blue = random.nextInt(81);
+        return Color.rgb(red, green, blue);
+    }
+
+    public int changeWarmColor() {
+        Random random = new Random();
+        int red3 = random.nextInt(81);
+        int green3 = random.nextInt(151) + 50;
+        int blue3 = random.nextInt(106) + 150;
+        return Color.rgb(red3, green3, blue3);
+    }
     private void checkResult(boolean isHigh) {
         TextView txtViewQuestion = findViewById(R.id.question);
         TextView txtViewAnswer = findViewById(R.id.answer);
@@ -99,9 +118,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (question < answer) {
                 result = "WIN";
                 score = 2;
+                int warmColor = generateRandomWarmColor();
+                txtViewAnswer.setBackgroundColor(warmColor);
             } else if (question > answer) {
                 result = "LOSE";
                 score = -1;
+                TextView txtView = findViewById(R.id.question);
+                Random rnd = new Random();
+                int warmColor = changeWarmColor();
+                txtViewQuestion.setBackgroundColor(warmColor);
             } else {
                 result = "DRAW";
                 score = 1;
@@ -110,9 +135,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (question > answer) {
                 result = "WIN";
                 score = 2;
+                int warmColor = generateRandomWarmColor();
+                txtViewAnswer.setBackgroundColor(warmColor);
             } else if (question < answer) {
                 result = "LOSE";
                 score = -1;
+                TextView txtView = findViewById(R.id.question);
+                Random rnd = new Random();
+                int warmColor = changeWarmColor();
+                txtViewQuestion.setBackgroundColor(warmColor);
             } else {
                 result = "DRAW";
                 score = 1;
