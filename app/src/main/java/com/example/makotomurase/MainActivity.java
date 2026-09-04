aaddcf146363c779e7c80642a71cebf6fe6b7622
n1497 <n1497@sobal.co.jp>
2026-09-04 13:13:03 +0900

    トップ画面に説明文を追加しました

--- app/src/main/java/com/example/makotomurase/MainActivity.java
+++ app/src/main/java/com/example/makotomurase/MainActivity.java
@@ -82,6 +82,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             setQuestionValue();
             clearAnswerValue();
             clearScoreValue();
+
+            TextView textViewQ = (TextView) findViewById(R.id.question);
+            textViewQ.setBackgroundColor(Color.parseColor("#ff00ff"));
+
+            TextView textViewA = (TextView) findViewById(R.id.answer);
+            textViewA.setBackgroundColor(Color.parseColor("#ffff00"));
+
         }
     }
 
